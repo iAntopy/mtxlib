f9112e0ecd42125c78709182c444e37675e43543
iAntopy <i-antony@hotmail.com>
2022-06-13 05:56:29 -0400

    added mtx_init_as_array() to copy an array's data inside a mtx->arr

---
+++ mtx_set.c
@@ -0,0 +1,11 @@
+
+#include "libft.h"
+#include "mtxlib.h"
+
+int	mtx_init_mtx_array(t_mtx *mtx, void *arr, int rows, int cols)
+{
+	if (!mtx->arr || mtx->shape[0] != rows || mtx->shape[1] != cols)
+		return (-1);
+	ft_memcpy(mtx->arr, arr, mtx->stride[0] * mtx->shape[0]);
+	return (1);
+}
